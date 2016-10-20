740b3836a4c981a65ca3c58b7075cd7256bfb5b8
Thomas <Thomas@192.168.0.128>
2016-10-20 20:41:19 +0200

    fixing own branch

--- SoftwareProject2/src/gitTest/GitTest.java
+++ SoftwareProject2/src/gitTest/GitTest.java
@@ -6,14 +6,10 @@ public class GitTest {
 		System.out.println("Test");
 		System.out.println("Artur");
 		System.out.println("Denis");
-<<<<<<< HEAD
 		System.out.println("Thomas");
-=======
-		System.out.print("hallo");
-		System.out.print("Juan is triggered");
-		System.out.print("halloooo");
+		System.out.println("hallo");
+		System.out.println("Juan is triggered");
+		System.out.println("halloooo");
 		System.out.println("Sebas G nog hier");
->>>>>>> refs/heads/master
 	}
-
 }
