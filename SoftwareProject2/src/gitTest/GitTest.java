4f051cdbee21299e999ff2be022875daf0e5ca73
Sebastian G <Sebastian G@Sebas.ehb.local>
2016-10-20 20:49:16 +0200

    println toegevoegd tijdens skype vergadering

--- SoftwareProject2/src/gitTest/GitTest.java
+++ SoftwareProject2/src/gitTest/GitTest.java
@@ -10,7 +10,7 @@ public class GitTest {
 		System.out.print("Juan is triggered");
 		System.out.print("halloooo");
 		System.out.println("Sebas G nog hier");
-		
+		System.out.println("Sebastian ook op skype");
 	}
 
 }
